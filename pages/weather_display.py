a70d74ba5afdc2d67a468c6a244b9d1b8cb1b88e
Thiago Moreira Gonçalves <thiago@siira.world>
2023-08-26 23:02:25 +0100

    new page

--- pages/weather_display.py
+++ pages/weather_display.py
@@ -1,9 +1,10 @@
 import requests
 import streamlit as st
+import pandas as pd
 
 
 # Streamlit app title
-st.title("Weather")
+st.title("Clima")
 
 # User input for city name
 city = st.text_input("Cidade:")
@@ -25,13 +26,29 @@ if city:
 
     # Extract temperature information from JSON response
     if response.status_code == 200:
+
+        lat = data["coord"]["lat"]
+        lon = data["coord"]["lon"]
+
+        df = pd.DataFrame({
+            'LAT': [lat], 
+            'lon': [lon]
+        })
+
+        st.map(df)
+
+
         temperature = data["main"]["temp"]
         feels_like = data["main"]["feels_like"]
         description = data["weather"][0]["description"]
+        temp_max = data["main"]["temp_max"]
+        temp_min = data["main"]["temp_min"]
 
         # Display weather information to the user
-        st.write(f"Temperatura em {city}: {temperature}°C")
+        st.write(f"Temperatura atual em {city}: {temperature}°C")
+        st.write(f"Max: {temp_max} - Min: {temp_min}" )
         st.write(f"Sensação: {feels_like}°C")
         st.write(f"Descrição: {description}")
+
     else:
-        st.write("Error: Unable to retrieve weather information.")
+        st.write("Error: Não foi possível retornar informações sobre o clima.")
